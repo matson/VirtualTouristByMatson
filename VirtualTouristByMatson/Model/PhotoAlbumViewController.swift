66632893d814519f5b3d8244aee2a8abc0168ebe
Madison <madison.k.adams@gmail.com>
2023-12-20 22:57:51 -0500

    added rows to collectionView and back button

--- VirtualTouristByMatson/Model/PhotoAlbumViewController.swift
+++ VirtualTouristByMatson/Model/PhotoAlbumViewController.swift
@@ -20,6 +20,11 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate, UICo
     var images: [UIImage] = []
     
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,27 +32,15 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate, UICo
         photoView.delegate = self
         photoView.dataSource = self
         
-        let space:CGFloat = 3.0
-        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+        setRows()
         
-        flowLayout.minimumInteritemSpacing = space
-        flowLayout.minimumLineSpacing = space
-        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
-        
-        // Convert the imageData to UIImage and populate the images array
-        if let imageData = imageData {
-                for data in imageData {
-                    if let image = UIImage(data: data) {
-                        images.append(image)
-                    }
-                }
-            }
+        makeImages()
         
         // Reload the collection view to display the images
         photoView.reloadData()
         
     }
-  
+    
     @IBAction func newCollection(_ sender: UIButton) {
         
     }
@@ -56,6 +49,15 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate, UICo
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.bounds.width
+        let space: CGFloat = 3.0
+        let numberOfItemsPerRow: CGFloat = 3
+        let dimension = (collectionViewWidth - ((numberOfItemsPerRow - 1) * space)) / numberOfItemsPerRow
+
+        return CGSize(width: dimension, height: dimension)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -68,6 +70,47 @@ class PhotoAlbumViewController: UIViewController, UICollectionViewDelegate, UICo
         cell.photo.image = imageData
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // Get the selected item's index path
+        let selectedIndexPath = indexPath
+        
+        // Perform the removal logic here
+        // For example, you can remove the item from your data source array
+//        dismiss(animated: true, completion: nil)
+        
+        // Reload the collection view to reflect the changes
+        collectionView.reloadData()
+    }
+    
+    func setRows(){
+        // Create a new flow layout instance
+        let layout = UICollectionViewFlowLayout()
+        
+        // Set the spacing between cells
+        layout.minimumInteritemSpacing = 3.0
+        layout.minimumLineSpacing = 3.0
+        
+        // Calculate the item size based on the available width
+        let collectionViewWidth = photoView.bounds.width
+        let numberOfItemsPerRow: CGFloat = 3
+        let dimension = (collectionViewWidth - ((numberOfItemsPerRow - 1) * layout.minimumInteritemSpacing)) / numberOfItemsPerRow
+        layout.itemSize = CGSize(width: dimension, height: dimension)
+        
+        // Set the collection view's layout
+        photoView.collectionViewLayout = layout
+    }
+    
+    func makeImages(){
+        // Convert the imageData to UIImage and populate the images array
+        if let imageData = imageData {
+            for data in imageData {
+                if let image = UIImage(data: data) {
+                    images.append(image)
+                }
+            }
+        }
     }
 }
 
